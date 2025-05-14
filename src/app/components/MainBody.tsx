ae5868eba7717b763687f8648dbe400c1003784e
A D <adsh20630@gmail.com>
2025-05-13 23:43:11 -0400

    Image adjustments

--- src/app/components/MainBody.tsx
+++ src/app/components/MainBody.tsx
@@ -4,7 +4,7 @@ export default function MainBody()
     return(
 <div className="content-center p-4 relative z-20 isolate">
     <h1 className="font-sans flex gap-2 mb-2">About Me</h1>
-        <p className="font-sans flex gap-2 mb-2">Lorem Ipsum</p>
+        <p className="font-sans flex gap-2 mb-2">I'm a software developer with a focus on web and Android development. Graduated from Augusta University with a Bachelor's in Computer Science in May 2025.</p>
 </div>
 )
 }
--- src/app/components/MidImage.tsx
+++ src/app/components/MidImage.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
- 
+import Image from "next/image";
+
 export default function MidImage() {
   return (
     <div className="grid-element inset-shadow-sm/50 relative after:absolute after:inset-0 after:bg-sky-300 after:mix-blend-soft-light after:rounded-lg relative z-5">
-<Image className="font-sans w-full h-64 object-cover grayscale" src="cybercenter.jpg" alt="" />
+      <Image
+        className="font-sans w-full h-64 object-cover grayscale"
+        src="cybercenter.jpg"
+        alt=""
+        width={1200}
+        height={300}
+        layout="responsive"
+      />
     </div>
-  )
+  );
 }
