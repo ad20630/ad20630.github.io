9a3fb6d2643824afb3c833dd849046198f21a4d9
A D <adsh20630@gmail.com>
2025-05-15 21:31:50 -0400

    contact form changes, favicon update

--- src/app/components/MidImage.tsx
+++ src/app/components/MidImage.tsx
@@ -4,11 +4,11 @@ export default function MidImage() {
   return (
     <div className="grid-element inset-shadow-sm/50 relative after:absolute after:inset-0 after:bg-sky-300 after:mix-blend-soft-light after:rounded-lg relative z-5">
       <Image
-        className="font-sans w-full h-64 object-cover grayscale"
+        className="font-sans w-full h-48 object-cover grayscale"
         src="cybercenter.jpg"
         alt=""
         width={1200}
-        height={300}
+        height={200}
         layout="responsive"
       />
     </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,22 +9,27 @@ export default function Home() {
       <MidImage />
       <div className="content-center p-4 relative z-20 isolate">
         <h1 className="font-sans text-xl flex gap-2 mb-2">Contact</h1>
-        <p className="font-sans flex gap-2 mb-2">
-          You can contact me using the form below, or via email at
-          ashire36@proton.me.
+        <p className="font-sans flex mb-2">
+          You can contact me via email at&nbsp;<a href="mailto:ashire36@proton.me" className="no-underline hover:underline text-sky-400">ashire36@proton.me</a>, or using the contact form below.
         </p>
       </div>
-      <Form className="" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeDTsdn2zxvSad-waZ3ja4IIoan7DIX786j-H6y4H8GKR-Tww/formResponse">
-        <label>Name</label>
-        <input name="entry.1901982553" type="text" required/>
+      <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeDTsdn2zxvSad-waZ3ja4IIoan7DIX786j-H6y4H8GKR-Tww/formResponse">
+        <div>
+          <label className="mr-2 pr-2">Name</label>
+          <input className="mb-4 w-1/6 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="entry.1901982553" type="text" required/>
+        </div>
 
-        <label>Email</label>
-        <input name="entry.1322536053" type="email" required />
+        <div>
+          <label className="mr-2 pr-2">Email</label>
+          <input className="mb-4 w-1/6 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"  name="entry.1322536053" type="email" required />
+        </div>
 
-        <label>Message</label>
-        <input name="entry.736781597" type="text" required />
+        <div>
+          <label className="mr-2 pr-2">Message</label>
+          <input  className="mb-4 w-1/6 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="entry.736781597" type="text" required />
+        </div>
 
-        <input type="submit" value="Send" />
+        <div><input className= "bg-transparent hover:bg-sky-500 text-sky-700 font-semibold hover:text-white py-2 px-4 border border-sky-500 hover:border-transparent rounded" type="submit" value="Send" /></div>
       </Form>
     </div>
   );
