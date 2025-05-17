a79497a343171d014a3176f3e981eb2a39c197c3
A D <adsh20630@gmail.com>
2025-05-17 15:10:25 -0400

    Fixed wrong label

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Header />
       <MidImage />
       <div className="content-center p-4 relative z-20 isolate">
-        <h1 className="font-sans text-xl flex gap-2 mb-2">Contact</h1>
+        <h1 className="font-sans text-xl flex gap-2 mb-2">Portfolio</h1>
         <p className="font-sans flex mb-2">
           In construction, coming soon
         </p>
