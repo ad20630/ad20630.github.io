b7a3c447504b01dadb6cd4cdb31198e8c983a220
A D <adsh20630@gmail.com>
2025-06-10 14:45:16 -0400

    added navbar for mobile view

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,45 +1,76 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header>
+    <header className="bg-emerald-100 dark:bg-emerald-200 shadow-lg relative z-10">
       <div
-        className="justify-between items-center w-full lg:flex lg:w-auto lg:order-1 bg-emerald-100 dark:bg-emerald-200 p-4 shadow-lg relative z-10"
+        className="flex items-center justify-between w-full lg:flex lg:w-auto lg:order-1 bg-emerald-100 dark:bg-emerald-200 p-4 shadow-lg relative z-10"
         id="mobile-menu-2"
       >
-        <ul className="flex items-center flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-          <li>
-            <a
-              className="font-sans text-lg block py-2 pr-4 pl-3 text-gray rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-500 flex items-center"
-            >
-              Aden Shire
-            </a>
-          </li>
-          <li>
-            <Link
-              href="/"
-              className="font-sans block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 flex items-center"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/portfolio"
-              className="font-sans block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Portfolio
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/contact"
-              className="font-sans block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
+        <div className="flex items-center">
+          <a
+            href="/"
+            className="font-sans text-lg block py-2 pr-4 pl-3 mr-4 font-medium text-gray rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-gray-500 flex items-center"
+          >
+            Aden Shire
+          </a>
+          <div
+            className={`w-full lg:flex lg:w-auto lg:items-center ${
+              isOpen ? "block" : "hidden"
+            }`}
+            id="navbar"
+          >
+            <ul className="flex items-center flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 lg:ml-4">
+              <li>
+                <Link
+                  href="/"
+                  className="font-sans block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 flex items-center"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/portfolio"
+                  className="font-sans block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 flex items-center"
+                >
+                  Portfolio
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/contact"
+                  className="font-sans block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 flex items-center"
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-600 rounded-lg lg:hidden hover:bg-gray-200 dark:text-gray-700 dark:hover:bg-gray-300"
+          aria-controls="navbar"
+          aria-expanded={isOpen}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
       </div>
     </header>
   );
